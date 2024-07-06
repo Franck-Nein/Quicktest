a278d83586089d65fad831e3aeaeac14e6faff78
Franck-Nein <35365671+Franck-Nein@users.noreply.github.com>
2024-07-06 05:16:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const html = `
+        <div id="app">
+            <header>
+                <h1>Simple Website</h1>
+            </header>
+            <main>
+                <p>Created with JavaScript.</p>
+            </main>
+            <footer>
+                <p>2024</p>
+            </footer>
+        </div>
+    `;
+
+    document.body.innerHTML = html;
+});
+
